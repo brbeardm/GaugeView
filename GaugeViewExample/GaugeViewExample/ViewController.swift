ed44b3dda588fe5acead875f7cb6e3d6837b1555
Mathew Piccinato <mpiccinato@gmail.com>
2016-03-16 16:38:05 -0400

    Simple demo

--- GaugeViewExample/GaugeViewExample/ViewController.swift
+++ GaugeViewExample/GaugeViewExample/ViewController.swift
@@ -9,17 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var gaugeView: GaugeView?
+    @IBOutlet var label: UILabel?
+    @IBOutlet var slider: UISlider?
+    
+    @IBOutlet var decrement: UIButton?
+    @IBOutlet var increment: UIButton?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+       
+        self.gaugeView?.stops = [
+            (0.0, UIColor.greenColor()),
+            (0.25, UIColor.yellowColor()),
+            (0.5, UIColor.orangeColor()),
+            (0.75, UIColor.redColor())
+        ]
+        self.setProgress(Double(self.slider!.value))
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func setProgress(progress: Double) {
+        self.gaugeView?.progress = progress
+        self.label?.text = String(format: "%.2f", self.gaugeView!.progress)
+        self.slider?.setValue(Float(self.gaugeView!.progress), animated: false)
+    }
 
-
+    @IBAction func sliderValueDidChange(sender: UISlider) {
+        self.setProgress(Double(sender.value))
+    }
+    
+    @IBAction func decrementValue(sender: UIButton) {
+        self.setProgress(self.gaugeView!.progress - 0.1)
+    }
+    
+    @IBAction func incrementValue(sender: UIButton) {
+        self.setProgress(self.gaugeView!.progress + 0.1)
+    }
 }
 
